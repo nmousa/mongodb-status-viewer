1c4e2f1da6c6baddf7facb9b200dacef8e4494de
Nilab Mousa <nmousa@clearwateranalytics.com>
2017-06-19 15:14:09 -0600

    Delete everything

--- server/src/config/constants/constants.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Moiz.Kachwala on 15-06-2016.
- */
-
-class Constants {
-    static DB_CONNECTION_STRING: string = process.env.NODE_ENV === 'production' ? process.env.dbURI : "mongodb://localhost:27017/quickStart"
-}
-Object.seal(Constants);
-export = Constants;
