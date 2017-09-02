2d7719c80a6227ba5c5b5d8ac9406e0f2bb668b9
nmousa <nmousa@onyx.boisestate.edu>
2017-09-02 14:03:06 -0600

    Fix router bug

--- server.js
+++ server.js
@@ -9,20 +9,19 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(bodyParser.urlencoded({ 'extended': 'false' }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// For express paths 
 app.use('/api', api);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// All of the other requests should go through Angular Router 
+app.get('*', function (req, res, next) {
+  res.sendfile('./dist/index.html');
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
